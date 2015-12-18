e2f2889a32be295c5fe998b0344a7ef061804593
Tomas Musilek <Tomas Musilek>
2015-12-18 14:09:33 +0100

    Added test suite

---
+++ src/test/java/com/musilto5/helloworldproject/SmokeTestSuite.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.musilto5.helloworldproject;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author tomas
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  FactorialTest.class
+})
+
+public class SmokeTestSuite {
+    
+}
